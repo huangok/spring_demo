683aef169123f238211b4cb9cdccad1379bac25b
feihuangk@isoftstone.com <feihuangk@isoftstone.com>
2017-11-02 14:26:19 +0800

    首次提交

---
+++ spring_demo/src/main/java/com/huangok/spring_demo/App.java
@@ -0,0 +1,13 @@
+package com.huangok.spring_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
